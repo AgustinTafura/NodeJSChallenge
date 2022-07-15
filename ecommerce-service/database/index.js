cf92a04839b24fb181fb3222ed5f40698240fa78
AgustinTafura <agustintafura@hotmail.com>
2022-07-15 15:41:09 -0300

    create database if not exist

--- ecommerce-service/database/index.js
+++ ecommerce-service/database/index.js
@@ -4,44 +4,67 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const config = require(__dirname + '/../config/config').development;
+const config = require(__dirname + '/../config/config')[process.env.NODE_ENV || 'development'];
+const mysql = require('mysql2/promise');
 const pathModels = __dirname + '/../models'
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-  dialect: config.dialect,
-  // host: config.host,
-  logging: false,
-});
-fs
-  .readdirSync(pathModels)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(pathModels, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
+
+initialize();
+
+async function initialize() {
+
+  const { host, port, username, password, database, dialect } = config;
+
+  // create db if it doesn't already exist
+  const connection = await mysql.createConnection({
+    host:process.env.NODE_ENV ? host : 'localhost',
+    user:username,
+    password,
+    });
+  await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+
+  // connect to db
+  const sequelize = new Sequelize(database, username, password, {
+    dialect,
+    // host // USE ONLY WITH DOCKER-COMPOSE MYSQL SERVICE
+    // logging,
   });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+  // init models and add them to the exported db object
+  fs
+    .readdirSync(pathModels)
+    .filter(file => {
+      return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+    .forEach(file => {
+      const model = require(path.join(pathModels, file))(sequelize, Sequelize.DataTypes);
+      db[model.name] = model;
+    });
+  
+  Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+      db[modelName].associate(db);
+    }
+  });
 
-
-sequelize.sync({
-  alter: false,
-})
+  // sync all models with database
+  sequelize.sync({
+    alter: false,
+  })
   .then(function() {
     console.log('DB connection sucessful.');
   })
   .catch(error => {
     console.log("Couldn't sync database", error);
   });
+  
+  db.sequelize = sequelize;
+  db.Sequelize = Sequelize;
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+}
+
+
 
 
 module.exports = db;
