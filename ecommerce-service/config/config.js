30fb7d65c94a81a2d42b2128210b959ad9f591c6
AgustinTafura <agustintafura@hotmail.com>
2022-07-15 15:41:48 -0300

    testing dependencies

--- ecommerce-service/config/config.js
+++ ecommerce-service/config/config.js
@@ -4,26 +4,25 @@ module.exports = {
     "development": {
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
-        "database":  process.env.DB_DATABASE,
-        "host": process.env.DB_NAME, // USE ONLY WITH DOCKER-COMPOSE MYSQL SERVICE
+        "database":  `${process.env.DB_DATABASE}_dev`,
+        "host": process.env.DB_SERVICE_NAME, // USE ONLY WITH DOCKER-COMPOSE MYSQL SERVICE else USE 'localhost'
         "port": process.env.DB_PORT,
         "dialect": process.env.DB_DIALECT,
-        "logging": false,
     },
     "test": {
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
-        "database":  process.env.ECOMMERCE_MYSQL_DATABASE,
-        "host": process.env.ECOMMERCE_MYSQL_NAME, // USE ONLY WITH DOCKER-COMPOSE MYSQL SERVICE
-        "port": process.env.ECOMMERCE_MYSQL_PORT,
+        "database":  `${process.env.DB_DATABASE}_test`,
+        "host": 'localhost', // USE ONLY WITH DOCKER-COMPOSE MYSQL SERVICE else USE 'localhost'
+        "port": process.env.DB_PORT,
         "dialect": process.env.DB_DIALECT
     },
     "production": {
         "username": process.env.DB_USER,
         "password": process.env.DB_PASSWORD,
-        "database":  process.env.ECOMMERCE_MYSQL_DATABASE,
-        "host": process.env.ECOMMERCE_MYSQL_NAME, // USE ONLY WITH DOCKER-COMPOSE MYSQL SERVICE
-        "port": process.env.ECOMMERCE_MYSQL_PORT,
+        "database":  process.env.DB_DATABASE,
+        "host": process.env.DB_SERVICE_NAME, // USE ONLY WITH DOCKER-COMPOSE MYSQL SERVICE else USE 'localhost'
+        "port": process.env.DB_PORT,
         "dialect": process.env.DB_DIALECT
     }
 }
