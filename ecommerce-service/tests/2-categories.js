a09b8e3e291ff6a1649fba379a1c3d7fb89022bb
AgustinTafura <agustintafura@hotmail.com>
2022-07-30 15:45:37 -0300

    test categories updated

--- ecommerce-service/tests/2-categories.js
+++ ecommerce-service/tests/2-categories.js
@@ -33,86 +33,135 @@ setTimeout(function() {
             name: "category test",
             description: "description test"
         };    
-        
-        it('Should create a new Category', (done) => {
-            chai
-            .request(app)
-            .post('/categories')
-            .set({ Authorization: `Bearer ${defaultUser.token}` })
-            .send(newCategory)
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.have.property('id');
-                res.body.should.have.property('name');
-                res.body.should.have.property('description');
-                newCategory.id = res.body.id
-                done();
-            });
-        });
 
-        it('Should get a Category by Id', (done) => {
-            chai
-            .request(app)
-            .get(`/categories/${newCategory.id}`)
-            .set({ Authorization: `Bearer ${defaultUser.token}` })
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.have.property('id');
-                res.body.should.have.property('name');
-                res.body.should.have.property('description');
-                res.body.should.have.property('Products');
-                res.body.Products.should.be.a('array')
+        context('using correct data', function () {
 
-                done();
-            });
-        });
-        
-        it('Should update a categories', (done) => {
-
-            const changes = {
-                name: 'test name updated'
-            }
             
-            chai
-            .request(app)
-            .get(`/categories/${newCategory.id}`)
-            .set({ Authorization: `Bearer ${defaultUser.token}` })
-            .send(changes)
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.have.property('id');
-                res.body.should.have.property('name');
-                res.body.should.have.property('description');
-                res.body.should.have.property('Products');
-                res.body.Products.should.be.a('array')
-                done();
+            it('Should create a new Category', (done) => {
+                chai
+                .request(app)
+                .post('/categories')
+                .set({ Authorization: `Bearer ${defaultUser.token}` })
+                .send(newCategory)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('id');
+                    res.body.should.have.property('name');
+                    res.body.should.have.property('description');
+                    newCategory.id = res.body.id
+                    done();
+                });
             });
-        });
 
-        it('Should return all Categories', (done) => {
-            chai
-            .request(app)
-            .get('/categories')
-            .set({ Authorization: `Bearer ${defaultUser.token}` })
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('array')
-                done();
+            it('Should get a Category by Id', (done) => {
+                chai
+                .request(app)
+                .get(`/categories/${newCategory.id}`)
+                .set({ Authorization: `Bearer ${defaultUser.token}` })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('id');
+                    res.body.should.have.property('name');
+                    res.body.should.have.property('description');
+                    res.body.should.have.property('Products');
+                    res.body.Products.should.be.a('array')
+
+                    done();
+                });
             });
-        });
+            
+            it('Should update a categories', (done) => {
 
-        it('Should delete Category by Id', (done) => {
-            chai
-            .request(app)
-            .delete(`/categories/${newCategory.id}`)
-            .set({ Authorization: `Bearer ${defaultUser.token}` })
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.text.should.include('eliminado');
-                done();
+                const changes = {
+                    name: 'test name updated'
+                }
+                
+                chai
+                .request(app)
+                .get(`/categories/${newCategory.id}`)
+                .set({ Authorization: `Bearer ${defaultUser.token}` })
+                .send(changes)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('id');
+                    res.body.should.have.property('name');
+                    res.body.should.have.property('description');
+                    res.body.should.have.property('Products');
+                    res.body.Products.should.be.a('array')
+                    done();
+                });
             });
-        });
 
+            it('Should return all Categories', (done) => {
+                chai
+                .request(app)
+                .get('/categories')
+                .set({ Authorization: `Bearer ${defaultUser.token}` })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('array')
+                    done();
+                });
+            });
+
+            it('Should delete Category by Id', (done) => {
+                chai
+                .request(app)
+                .delete(`/categories/${newCategory.id}`)
+                .set({ Authorization: `Bearer ${defaultUser.token}` })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.text.should.include('eliminado');
+                    done();
+                });
+            });
+        })
+
+        context('using invalid data', function () {
+
+            it('Should return error. Category alredy exists', (done) => {
+                chai
+                .request(app)
+                .post('/categories')
+                .set({ Authorization: `Bearer ${defaultUser.token}` })
+                .send(newCategory)
+                .end((err, res) => {
+                    res.should.have.status(500);
+                    // res.text.should.include('La categoria ya existe');
+                    done();
+                });
+            });
+
+            it('Should return error. Creating category without required parameter', (done) => {
+                const badCategory = {
+                    description: 'test'
+                }
+                chai
+                .request(app)
+                .post('/categories')
+                .set({ Authorization: `Bearer ${defaultUser.token}` })
+                .send(badCategory)
+                .end((err, res) => {
+                    res.should.have.status(500);
+                    // res.text.should.include('WHERE parameter "name" has invalid "undefined" value');
+                    done();
+                });
+            });
+            
+            it('Should return error. Category id not exist', (done) => {
+                chai
+                .request(app)
+                .get(`/categories/0`)
+                .set({ Authorization: `Bearer ${defaultUser.token}` })
+                .end((err, res) => {
+                    res.should.have.status(404);
+                    res.text.should.include('Error: La categoria no existe');
+                    done();
+                });
+            });
+
+            
+        })
     })
 
     run()   
